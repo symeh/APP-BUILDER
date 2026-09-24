b1432fd4ed76e92ec815c907006056eacf54cc66
syed <syedzeeshan198237@gmail.com>
2026-09-24 16:14:20 +0500

    Configure Vercel API handler

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,5 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../artifacts/api-server/src/app";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
+  return (app as any)(req, res);
 }
